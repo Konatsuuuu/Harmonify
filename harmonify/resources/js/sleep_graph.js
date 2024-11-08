988a14e48af3a718ea4e3178438ae56691c8e143
haruharu29 <156629469+haruharu29@users.noreply.github.com>
2024-11-08 11:22:05 +0900

    SleepGraph

--- harmonify/resources/js/sleep_graph.js
+++ harmonify/resources/js/sleep_graph.js
@@ -3,4 +3,6 @@ import { createApp } from "vue";
 import SleepPage from "./components/SleepGraph.vue";
 import "../css/app.css";
 
+import "flowbite"; // Import Flowbite JS file here
+
 createApp(SleepPage).mount("#sleep-app");
