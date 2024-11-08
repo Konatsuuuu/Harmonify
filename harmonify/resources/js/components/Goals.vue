a973156adf5da9da2c48b2c5ecf55b9d925327c4
haruharu29 <156629469+haruharu29@users.noreply.github.com>
2024-11-08 11:22:05 +0900

    SleepGraph

--- harmonify/resources/js/components/Goals.vue
+++ harmonify/resources/js/components/Goals.vue
@@ -1,17 +1,35 @@
 <template>
     <div class="flex flex-col items-center mt-4">
-        <v-progress-circular :model-value="value" :size="200" :width="30" color="teal" class="relative"
-            style="top: -40px">
+        <v-progress-circular
+            :model-value="value"
+            :size="200"
+            :width="30"
+            color="teal"
+            class="relative"
+            style="top: -40px"
+        >
             <template v-slot:default> {{ value }} % </template>
         </v-progress-circular>
 
-        <p class="text-4 font-bold text-[#282e2f] relative underline" style="top: -40px">
+        <p
+            class="text-4 font-bold text-[#282e2f] relative underline"
+            style="top: -40px"
+        >
             To-Do-List
         </p>
         <ul>
-            <li v-for="(task, index) in toDo" :key="index" class="relative mx-5" style="top: -40px">
-                <input type="checkbox" v-model="task.completed" @change="updateProgress"
-                    class="text-4 font-bold text-[#282e2f] mx-2 mt-5 relative" />
+            <li
+                v-for="(task, index) in toDo"
+                :key="index"
+                class="relative mx-5 mt-3"
+                style="top: -40px"
+            >
+                <input
+                    type="checkbox"
+                    v-model="task.completed"
+                    @change="updateProgress"
+                    class="text-4 font-bold text-[#282e2f] mx-2 relative"
+                />
                 <span :class="{ completed: task.completed }">{{
                     task.name
                 }}</span>
@@ -26,8 +44,8 @@ export default {
         return {
             toDo: [
                 { name: "Task 1", completed: false },
-                { name: "Task 2", completed: false, },
-                { name: "Task 3", completed: false, },
+                { name: "Task 2", completed: false },
+                { name: "Task 3", completed: false },
             ],
             value: 0,
         };
